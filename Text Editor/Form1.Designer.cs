74bc91befc8fc7f4c25f9112cbb61ca013849d9d
domen <domen@DESKTOP-0N7CJVN>
2022-05-10 13:03:27 +0200

    Add project files.

---
+++ Text Editor/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Text_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RTB = new System.Windows.Forms.RichTextBox();
+            this.CMBfile = new System.Windows.Forms.ComboBox();
+            this.CMBtools = new System.Windows.Forms.ComboBox();
+            this.BTNopen = new System.Windows.Forms.Button();
+            this.BTNsave = new System.Windows.Forms.Button();
+            this.BTNsaveas = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RTB
+            // 
+            this.RTB.Location = new System.Drawing.Point(12, 40);
+            this.RTB.Name = "RTB";
+            this.RTB.Size = new System.Drawing.Size(523, 398);
+            this.RTB.TabIndex = 0;
+            this.RTB.Text = "";
+            // 
+            // CMBfile
+            // 
+            this.CMBfile.FormattingEnabled = true;
+            this.CMBfile.Items.AddRange(new object[] {
+            "Open",
+            "Save",
+            "Save as"});
+            this.CMBfile.Location = new System.Drawing.Point(13, 13);
+            this.CMBfile.Name = "CMBfile";
+            this.CMBfile.Size = new System.Drawing.Size(121, 21);
+            this.CMBfile.TabIndex = 1;
+            this.CMBfile.SelectedIndexChanged += new System.EventHandler(this.CMBfile_SelectedIndexChanged);
+            // 
+            // CMBtools
+            // 
+            this.CMBtools.FormattingEnabled = true;
+            this.CMBtools.Items.AddRange(new object[] {
+            "Bold",
+            "Italic",
+            "Underline"});
+            this.CMBtools.Location = new System.Drawing.Point(154, 13);
+            this.CMBtools.Name = "CMBtools";
+            this.CMBtools.Size = new System.Drawing.Size(121, 21);
+            this.CMBtools.TabIndex = 2;
+            this.CMBtools.SelectedIndexChanged += new System.EventHandler(this.CMBtools_SelectedIndexChanged);
+            // 
+            // BTNopen
+            // 
+            this.BTNopen.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.BTNopen.Location = new System.Drawing.Point(584, 58);
+            this.BTNopen.Name = "BTNopen";
+            this.BTNopen.Size = new System.Drawing.Size(171, 72);
+            this.BTNopen.TabIndex = 3;
+            this.BTNopen.Text = "Open";
+            this.BTNopen.UseVisualStyleBackColor = true;
+            this.BTNopen.Click += new System.EventHandler(this.BTNopen_Click);
+            // 
+            // BTNsave
+            // 
+            this.BTNsave.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.BTNsave.Location = new System.Drawing.Point(584, 198);
+            this.BTNsave.Name = "BTNsave";
+            this.BTNsave.Size = new System.Drawing.Size(171, 72);
+            this.BTNsave.TabIndex = 4;
+            this.BTNsave.Text = "Save";
+            this.BTNsave.UseVisualStyleBackColor = true;
+            this.BTNsave.Click += new System.EventHandler(this.BTNsave_Click);
+            // 
+            // BTNsaveas
+            // 
+            this.BTNsaveas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.BTNsaveas.Location = new System.Drawing.Point(584, 331);
+            this.BTNsaveas.Name = "BTNsaveas";
+            this.BTNsaveas.Size = new System.Drawing.Size(171, 72);
+            this.BTNsaveas.TabIndex = 5;
+            this.BTNsaveas.Text = "Save as";
+            this.BTNsaveas.UseVisualStyleBackColor = true;
+            this.BTNsaveas.Click += new System.EventHandler(this.BTNnew_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BTNsaveas);
+            this.Controls.Add(this.BTNsave);
+            this.Controls.Add(this.BTNopen);
+            this.Controls.Add(this.CMBtools);
+            this.Controls.Add(this.CMBfile);
+            this.Controls.Add(this.RTB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox RTB;
+        private System.Windows.Forms.ComboBox CMBfile;
+        private System.Windows.Forms.ComboBox CMBtools;
+        private System.Windows.Forms.Button BTNopen;
+        private System.Windows.Forms.Button BTNsave;
+        private System.Windows.Forms.Button BTNsaveas;
+    }
+}
+
---
+++ Text Editor/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Text_Editor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BTNopen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string text = File.ReadAllText(openFileDialog.FileName);
+                RTB.Text = text;
+            }
+        }
+
+        private void BTNsave_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(@"D:\Visual Studio\C#\Text Editor\mojaDatoteka.txt", RTB.Text);
+        }
+
+        private void BTNnew_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            try
+            {
+                sfd.Filter = "Text File|*.txt";
+                sfd.FileName = "";
+                sfd.Title = "Save Text File";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    String path = sfd.FileName;
+                    StreamWriter  writer = new StreamWriter(File.Create(path));
+                    try
+                    {
+                        writer.Write(RTB.Text);
+                    }
+                    finally
+                    {
+                        writer.Dispose();
+                        RTB.Clear();
+                    }
+                }
+            }
+            finally
+            {
+                sfd.Dispose();
+            }
+        }
+
+        private void CMBtools_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string option = CMBtools.Text;
+
+            if (option == "Bold")
+            {
+                Font new1, old1;
+                old1 = RTB.SelectionFont;
+                if (old1.Bold)
+                    new1 = new Font(old1, old1.Style & ~FontStyle.Bold);
+                else
+                    new1 = new Font(old1, old1.Style | FontStyle.Bold);
+
+                RTB.SelectionFont = new1;
+                RTB.Focus();
+            }
+
+            if (option == "Italic")
+            {
+                Font new1, old1;
+                old1 = RTB.SelectionFont;
+                if (old1.Italic)
+                    new1 = new Font(old1, old1.Style & ~FontStyle.Italic);
+                else
+                    new1 = new Font(old1, old1.Style | FontStyle.Italic);
+
+                RTB.SelectionFont = new1;
+
+                RTB.Focus();
+            }
+
+            if (option == "Underline")
+            {
+                Font new1, old1;
+                old1 = RTB.SelectionFont;
+                if (old1.Underline)
+                    new1 = new Font(old1, old1.Style & ~FontStyle.Underline);
+                else
+                    new1 = new Font(old1, old1.Style | FontStyle.Underline);
+
+                RTB.SelectionFont = new1;
+                RTB.Focus();
+            }
+        }
+
+        private void CMBfile_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string option = CMBfile.Text;
+            if(option == "Open")
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string text = File.ReadAllText(openFileDialog.FileName);
+                    RTB.Text = text;
+                }
+            }
+
+            if (option == "Save")
+            {
+                File.WriteAllText(@"D:\Visual Studio\C#\Text Editor\JustSave.txt", RTB.Text);
+            }
+
+            if (option == "Save as")
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                try
+                {
+                    sfd.Filter = "Text File|*.txt";
+                    sfd.FileName = "";
+                    sfd.Title = "Save Text File";
+                    if (sfd.ShowDialog() == DialogResult.OK)
+                    {
+                        String path = sfd.FileName;
+                        StreamWriter writer = new StreamWriter(File.Create(path));
+                        try
+                        {
+                            writer.Write(RTB.Text);
+                        }
+                        finally
+                        {
+                            writer.Dispose();
+                            RTB.Clear();
+                        }
+                    }
+                }
+                finally
+                {
+                    sfd.Dispose();
+                }
+            }
+        }
+    }
+}
